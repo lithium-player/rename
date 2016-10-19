67220178df357cc2d628379b1f3f4f1b6ce73356
David Barnett <david@barnett.net.nz>
2016-10-20 10:10:01 +1300

    feat(cli): add dryrun/move/copy arguments

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use walkdir::WalkDir;
 use liquery::{Query, EvalFunc};
 use liquery_file::QueryFile;
 
+use std::io::Error as IOError;
+use std::path::Path;
+
 fn main() {
 
     let app = cli::build_cli().get_matches();
@@ -34,12 +37,39 @@ fn main() {
                 if metadata.is_file() {
                     let queryable = QueryFile::new(e.path()).unwrap();
 
-                    println!("{} => {}",
-                             e.path().to_str().unwrap(),
-                             query.eval(&queryable, &func).unwrap());
-
+                    if app.is_present("MOVE") {
+                        let _ = move_file(e.path(), query.eval(&queryable, &func).unwrap());
+                    } else if app.is_present("COPY") {
+                        let _ = copy_file(e.path(), query.eval(&queryable, &func).unwrap());
+                    } else {
+                        let _ = dry_run_move(e.path(), query.eval(&queryable, &func).unwrap());
+                    }
                 }
             }
         }
     }
+}
+
+fn dry_run_move<F, T>(from: F, to: T) -> Result<(), IOError>
+    where F: AsRef<Path>,
+          T: AsRef<Path>
+{
+    println!("{:?} => {:?}", from.as_ref(), to.as_ref());
+    Ok(())
+}
+
+fn move_file<F, T>(from: F, to: T) -> Result<(), IOError>
+    where F: AsRef<Path>,
+          T: AsRef<Path>
+{
+    println!("moving {:?} => {:?}", from.as_ref(), to.as_ref());
+    Ok(())
+}
+
+fn copy_file<F, T>(from: F, to: T) -> Result<(), IOError>
+    where F: AsRef<Path>,
+          T: AsRef<Path>
+{
+    println!("copying {:?} => {:?}", from.as_ref(), to.as_ref());
+    Ok(())
 }
